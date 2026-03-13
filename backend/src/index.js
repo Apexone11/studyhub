ab01ce434102027d7999b1a587025df9f69a802b
Apexone11 <afornah1@umbc.edu>
2026-03-12 22:53:35 -0400

    fix: correct template string syntax in server log message

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log('Server running on http://localhost:${PORT}')
+    console.log(`Server running on http://localhost:${PORT}`)
 })
 
 
