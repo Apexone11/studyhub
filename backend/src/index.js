f61328d935b4b7e124ebecdde87be8aec0bc0ce4
Apexone11 <afornah1@umbc.edu>
2026-03-13 00:19:36 -0400

    feat: add authentication routes to the API

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,12 @@ require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 4000
+const authRoutes = require('./routes/auth')
 
 // middleware
 app.use(cors({ origin: 'Http://localhost:5173', credentials: true}))
 app.use(express.json())
+app.use('/api/auth', authRoutes)
 
 //Health check route
 app.get('/', (req, res) => {
